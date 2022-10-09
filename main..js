2eaa6b72089ab23e681e47a16a3648cdc1051348
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-09 15:43:45 +0530

    file added

---
+++ main..js
@@ -0,0 +1,23 @@
+var dateofbirth=document.querySelector("#Date-of-birth");
+var luckynumber=document.querySelector("#Luck-number");
+var checkbutton=document.querySelector("#check");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+checkbutton.addEventListener("click",console.log("tap"));
