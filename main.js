1571f9164193c98f3b9144bd63fc4d873d5a644e
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-19 02:59:25 +0530

    j

--- main.js
+++ main.js
@@ -2,33 +2,41 @@ var dateofbirth=document.querySelector("#Date-of-birth");
 var luckynumber=document.querySelector("#Luck-number");
 var checkbutton=document.querySelector("#check");
 var outputresult=document.querySelector("#output")
-    const date=dateofbirth.value;
-    const luckynum=luckynumber.value; 
-checkbutton.addEventListener("click",function calculate(){
-    hypenremovedatesumtotal(date)
-});
-function hypenremovedatesumtotal(dob)
-{
-    
-dob=dob.replaceAll("-","")
-var  sum=0;
-for(let i=0;i<dob.length;i++)
-{
-sum=sum + Number(dob[i]);
-}
-finaloutput(sum)
-
-}
-function finaloutput(db)
-{
-    
-    if(db%Number(luckynum)==0)
-    { 
-    outputresult.innerText="Yay! Your birthday is lucky 🙂";
+    var  date=dateofbirth.value;
+    var luckynum=luckynumber.value; 
+    function sumDOB(){
+        var dob = date.replaceAll("-","")
+        var sum=0;
+        for(var i=0;i<dob.length;i++){
+            sum = sum + Number(dob[i])
+        }
+        return sum
     }
     
-    else{
-       
-        outputresult.innerText="Alas! Your birthday is not lucky 😌";
-       } 
-}
+    function isLuckyDob(sum){
+    
+        outputresult.style.display= "block";
+        if(sum % luckynum ===0){
+            outputresult.innerText = "Yay! Your birthday is lucky 🙂"
+        }else{
+            outputresult.innerText = "Alas! Your birthday is not lucky 😌"
+        }
+    }
+    
+    function checkNumberEventHandler(){
+        if(date == ""|| luckynum == "")
+        {
+         outputresult.innerText = "Enter the input"
+        
+           
+        }
+           else
+        {
+            const sum = sumDOB()
+            isLuckyDob(sum)
+        }
+        outputresult.style.display= "none";
+    }
+    
+    checkbutton.addEventListener("click",checkNumberEventHandler);
+
